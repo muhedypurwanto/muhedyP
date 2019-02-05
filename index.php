861a9d6c9dd942d4792690696f0e3939d239ac83
muhedypurwanto <47342558+muhedypurwanto@users.noreply.github.com>
2019-02-05 11:55:07 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<table width="419">
+
+<td width="298" height="377" align="right"> 
+   <p><strong> NAMA      : Muh Edy Purwanto</strong></p>>
+   <p><strong> NIM       : 1215280229</strong></p>
+   <p><strong> TGl LAHIR : 11 01 1995</strong></p>
+   <p><strong> EMAIL     : edy555257@gmail.com</strong></p>
+   <p><strong> ALAMAT    : SIDRAP-SULSEL</strong></p></td>
+</table>
